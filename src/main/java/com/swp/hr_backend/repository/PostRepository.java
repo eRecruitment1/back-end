572e6a1d05e42e40ff1b74cefb2893eeb005fb81
Thuong <thuongmc2k2@gmail.com>
2022-10-06 14:52:32 +0700

    fixsql

--- src/main/java/com/swp/hr_backend/repository/PostRepository.java
+++ src/main/java/com/swp/hr_backend/repository/PostRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import com.swp.hr_backend.entity.Post;
 @Repository
 public interface PostRepository extends PagingAndSortingRepository<Post,Integer>{
-    @Query(value = "SELECT TOP 6 * FROM dbo.post WHERE status = 1 ORDER BY post_id DESC" , nativeQuery = true)
+    @Query(value = "SELECT * FROM post WHERE status = true ORDER BY post_id DESC Limit 6 " , nativeQuery = true)
     public List<Post> getLastestPost();
     @Query("SELECT p FROM Post p WHERE p.title LIKE %?1% AND p.status = true")
     List<Post> findPostByTitle(String search, Sort sort);
