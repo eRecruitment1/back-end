f1f2d8822b1c19dfdf2951a9a2f69fd7ef9562e0
giathuong172 <thuongmc2k2@gmail.com>
2022-10-31 14:58:43 +0700

    Fix Get Schedule By HREmployee

--- src/main/java/com/swp/hr_backend/controller/ScheduleController.java
+++ src/main/java/com/swp/hr_backend/controller/ScheduleController.java
@@ -17,6 +17,9 @@ import javax.mail.MessagingException;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RequestMapping("/api/schedule")
@@ -60,5 +63,11 @@ public class ScheduleController {
     public ResponseEntity<String>  updateStatus() {
          return ResponseEntity.ok(scheduleService.loadStatus());
     }
+    @GetMapping(value="/getHRemployeeScheduleDetail")
+    public ResponseEntity<List<ScheduleDetailResponse>> getHREmployeeDetail() throws CustomUnauthorizedException {
+        List<ScheduleDetailResponse> scheduleDetailResponses = scheduleService.viewHREmployeeScheduleDetail();
+        return ResponseEntity.ok(scheduleDetailResponses);
+    }
+    
     
 }
